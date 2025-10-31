230eaceac7c611bd14aad90d6f931d8629f33caa
J <jean.chalard@gmail.com>
2025-10-31 21:25:06 +0900

    Improve the count_times script in various dimensions
    The new version will now count
    • the time on each occurrence of the grepped text, and display it
    • the total time on each day where the grepped text occurs
    • the number of days on which the grepped text occurs
    • the total time over the entire period

--- count_times.rb
+++ count_times.rb
@@ -10,22 +10,58 @@
 regexp = Regexp.new(ARGV.shift, true)
 dayR = /^\d\d-\d\d/
 
-day = nil
-done = true
-count = 0
-while l = gets
-  l.chomp!
-  case l
-  when dayR
-    day = l
-    done = false
-  when regexp
-    if !done
-      puts "#{day} : #{l}"
-      done = true
-      count += 1
+class String
+  def toMinutes
+    throw "Bug : expected line at format `\\d\\d\\d\\d <activity>` : #{self}" unless self.match(/(\d\d)(\d\d)/)
+    hours, minutes = $1.to_i, $2.to_i
+    hours * 60 + minutes
+  end
+end
+
+class Integer
+  def renderTime
+    if self < 60
+      "#{self}min"
+    else
+      "%dh%02d" % [self / 60, self % 60]
     end
   end
 end
 
-puts "Total : #{count}"
+day = nil
+done = true
+totalCount = 0
+dayCount = 0
+totalTime = 0
+todayTime = 0
+last = nil
+while l = gets
+  l.chomp!
+  if l.match(dayR)
+    day = l
+    done = false
+    puts "        ..." + todayTime.renderTime unless todayTime <= 0
+    todayTime = 0
+    raise "Not implemented : can't count time for last activity of the day" unless last.nil?
+  elsif !last.nil?
+    elapsed = l.toMinutes - last
+    puts " : #{elapsed.renderTime}"
+    todayTime += elapsed
+    totalTime += elapsed
+    last = nil
+  end
+  if l.match(regexp)
+    if !done
+      print "#{day} : #{l}"
+      done = true
+      dayCount += 1
+    else
+      print "     ...#{l}"
+    end
+    last = l.toMinutes
+    totalCount += 1
+  end
+end
+
+puts "Last item matched regexp, not counted in spent time" unless last.nil?
+puts "Total : #{totalCount} times over #{dayCount} days and #{totalTime.renderTime}"
